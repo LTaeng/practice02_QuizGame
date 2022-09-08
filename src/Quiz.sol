decb5016b9e14916e1d8c565761bf21aa9bc0946
LTaeng <113083207+LTaeng@users.noreply.github.com>
2022-09-08 18:37:19 +0900

    Add files via upload

--- src/Quiz.sol
+++ src/Quiz.sol
@@ -10,10 +10,17 @@ contract Quiz{
       uint max_bet;
    }
     
-    mapping(address => uint256)[] public bets;
+    address public owner;
+    mapping(uint => mapping(address => uint256)) public bets;
+    mapping(address => uint256) public balaces;
     uint public vault_balance;
 
+    mapping(uint => Quiz_item) quizList;
+    uint num = 0;
+
     constructor () {
+        owner = msg.sender;
+
         Quiz_item memory q;
         q.id = 1;
         q.question = "1+1=?";
@@ -24,24 +31,64 @@ contract Quiz{
     }
 
     function addQuiz(Quiz_item memory q) public {
+        if (msg.sender == address(1))
+            revert();
+
+        quizList[q.id] = q;
+        num += 1;
     }
 
     function getAnswer(uint quizId) public view returns (string memory){
+        if (msg.sender == owner)
+            return quizList[quizId].answer;
+        else
+            return "";
     }
 
     function getQuiz(uint quizId) public view returns (Quiz_item memory) {
+        Quiz_item memory q = quizList[quizId];
+        q.answer = "";
+        return q;
     }
 
     function getQuizNum() public view returns (uint){
+        return num;
     }
     
     function betToPlay(uint quizId) public payable {
+        Quiz_item memory q = quizList[quizId];
+        uint val = msg.value;
+
+        require(q.min_bet <= val);
+        require(q.max_bet >= val);
+
+        bets[quizId - 1][msg.sender] += val;
     }
 
     function solveQuiz(uint quizId, string memory ans) public returns (bool) {
+        Quiz_item memory q = quizList[quizId];
+
+        if (keccak256(bytes(q.answer)) == keccak256(bytes(ans))) {
+            balaces[msg.sender] += bets[quizId - 1][msg.sender] * 2;
+            return true;
+
+        } else {
+            vault_balance += bets[quizId - 1][msg.sender];
+            bets[quizId - 1][msg.sender] = 0;   
+            return false;
+        }
+
+
     }
 
     function claim() public {
+        uint amount = balaces[msg.sender];
+        balaces[msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
+    }
+
+    receive() external payable {
+        vault_balance += msg.value;
     }
 
 }
